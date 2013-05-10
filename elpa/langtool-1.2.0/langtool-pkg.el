dba4cb64f69dafc8692ad972fb90f6ed0dbc347a
Yatish Mehta <yatish.mehta@betterlabs.net>
2013-05-10 23:30:33 +0530

    Added elpa and vendor

---
+++ elpa/langtool-1.2.0/langtool-pkg.el
@@ -0,0 +1,1 @@
+(define-package "langtool" "1.2.0" "Grammer check utility using LanguageTool" (quote nil))
---
+++ elpa/svg-clock-0.4/svg-clock-autoloads.el
@@ -0,0 +1,29 @@
+;;; svg-clock-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (svg-clock) "svg-clock" "svg-clock.el" (20802 7973
+;;;;;;  268474 474000))
+;;; Generated autoloads from svg-clock.el
+
+(autoload 'svg-clock "svg-clock" "\
+Start/stop the svg clock.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("svg-clock-pkg.el") (20802 7973 350719
+;;;;;;  363000))
+
+;;;***
+
+(provide 'svg-clock-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; svg-clock-autoloads.el ends here
